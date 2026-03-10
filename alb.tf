b521ec343aa1f32ca4ef5fe90bc0e193ad6c9b8b
Siddu <siddalingswamyg42@gmail.com>
2026-03-10 20:27:58 +0000

    demo

--- alb.tf
+++ alb.tf
@@ -1,3 +1,7 @@
+###################################
+# APPLICATION LOAD BALANCER
+###################################
+
 resource "aws_lb" "dotnet_alb" {
 
   name               = "dotnet-bluegreen-lb"
@@ -12,7 +16,15 @@ resource "aws_lb" "dotnet_alb" {
     aws_subnet.subnet1.id,
     aws_subnet.subnet2.id
   ]
+
+  tags = {
+    Name = "dotnet-bluegreen-lb"
+  }
 }
+
+###################################
+# ALB LISTENER
+###################################
 
 resource "aws_lb_listener" "http" {
 
@@ -21,9 +33,25 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
+    type = "forward"
 
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    forward {
 
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = 1
+      }
+
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = 0
+      }
+
+      stickiness {
+        enabled  = false
+        duration = 1
+      }
+
+    }
   }
 }
